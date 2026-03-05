f334fe4befd67ec40a22f2ac219988c42d8cec6b
Rian Rahman Al Aziz <rianrahman8243@gmail.com>
2026-03-05 08:48:21 +0700

    Belajar Variabel

---
+++ project-pertama/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// variable
+
+func main() {
+	var firstName string = "jhon"
+
+	var lastName string
+	lastName = "wick"
+
+	fmt.Printf("halo %s %s!\n", firstName, lastName)
+}
