8b4b0f160d42afa6483709f697466e4cf5d1635d
Rian Rahman Al Aziz <rianrahman8243@gmail.com>
2026-03-12 08:46:30 +0700

    belajar type data string

---
+++ Pemrograman Go Dasar/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Rian"))
+	fmt.Println("Rian Rahman"[0])
+	fmt.Println("Rian Rahman Al aziz"[1])
+
+	fmt.Println("Rian")
+	fmt.Println("Rian Rahman")
+	fmt.Println("Rian Rahman Al aziz")
+}
