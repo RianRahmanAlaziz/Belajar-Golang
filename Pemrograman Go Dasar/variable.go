0cbb6e31c450ef8db28c97c8b19b3767af7f4011
Rian Rahman Al Aziz <rianrahman8243@gmail.com>
2026-03-12 09:16:33 +0700

    belajar variable

---
+++ Pemrograman Go Dasar/variable.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string
+
+	// name = "Rian Rahman"
+	// fmt.Println(name)
+
+	// name = "Rian Rahman al aziz"
+	// fmt.Println(name)
+
+	// var name = "Rian Rahman"
+	// fmt.Println(name)
+
+	// name = "Rian Rahman al aziz"
+	// fmt.Println(name)
+
+	// name := "Rian Rahman"
+	// fmt.Println(name)
+
+	// name = "Rian Rahman al aziz"
+	// fmt.Println(name)
+
+	var (
+		firstName = "rian"
+		LastName  = "rahman"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(LastName)
+}
